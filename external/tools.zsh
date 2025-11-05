eac64e1748de0586a0f9b7e23a7e423cf23f77ed
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-11-05 13:42:49 +0100

    chore (main): Update external tools configuration (added atuin, removed unnecessary comment)

--- external/tools.zsh
+++ external/tools.zsh
@@ -6,5 +6,5 @@ command -v thefuck >/dev/null 2>&1 && eval "$(thefuck --alias)"
 [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
 [[ -f "$HOME/.ghcup/env" ]] && . "$HOME/.ghcup/env"
 command -v oh-my-posh >/dev/null 2>&1 && eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/config-theme.toml)"
-#command -v atuin >/dev/null 2>&1 && eval "$(atuin init zsh)"
+command -v atuin >/dev/null 2>&1 && eval "$(atuin init zsh)"
 command -v zoxide >/dev/null 2>&1 && eval "$(zoxide init --cmd cd zsh)"
